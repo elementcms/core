77da4126d1332cd492e22f898f3de04a18747f25
Peter Day <peterday.main@gmail.com>
2020-02-18 17:25:00 +0000

    Element brand

--- src/views/dashboard.blade.php
+++ src/views/dashboard.blade.php
@@ -2,68 +2,7 @@
 @section('main')
 <div class="row-col">
 	<div class="col-lg b-r">
-		<div class="row no-gutter">
-			<div class="col-xs-6 col-sm-3 b-r b-b">
-				<div class="padding">
-					<div>
-						<span class="pull-right"><i class="fa fa-caret-up text-primary m-y-xs"></i></span>
-						<span class="text-muted l-h-1x"><i class="ion-ios-grid-view text-muted"></i></span>
-					</div>
-					<div class="text-center">
-						<h2 class="text-center _600">45</h2>
-						<p class="text-muted m-b-md">New Projects</p>
-						<div>
-							<span data-ui-jp="sparkline" data-ui-options="[2,3,2,2,1,3,6,3,2,1], {type:'line', height:20, width: '60', lineWidth:1, valueSpots:{'0:':'#818a91'}, lineColor:'#818a91', spotColor:'#818a91', fillColor:'', highlightLineColor:'rgba(120,130,140,0.3)', spotRadius:0}" class="sparkline inline"></span>
-						</div>
-					</div>
-				</div>
-	        </div>
-	        <div class="col-xs-6 col-sm-3 b-r b-b">
-				<div class="padding">
-					<div>
-						<span class="pull-right"><i class="fa fa-caret-up text-primary m-y-xs"></i></span>
-						<span class="text-muted l-h-1x"><i class="ion-document text-muted"></i></span>
-					</div>
-					<div class="text-center">
-						<h2 class="text-center _600">219</h2>
-						<p class="text-muted m-b-md">New Invoices</p>
-						<div>
-							<span data-ui-jp="sparkline" data-ui-options="[1,1,0,2,3,4,2,1,2,2], {type:'line', height:20, width: '60', lineWidth:1, valueSpots:{'0:':'#818a91'}, lineColor:'#818a91', spotColor:'#818a91', fillColor:'', highlightLineColor:'rgba(120,130,140,0.3)', spotRadius:0}" class="sparkline inline"></span>
-						</div>
-					</div>
-				</div>
-	        </div>
-	        <div class="col-xs-6 col-sm-3 b-r b-b">
-				<div class="padding">
-					<div>
-						<span class="pull-right"><i class="fa fa-caret-down text-danger m-y-xs"></i></span>
-						<span class="text-muted l-h-1x"><i class="ion-pie-graph text-muted"></i></span>
-					</div>
-					<div class="text-center">
-						<h2 class="text-center _600">8</h2>
-						<p class="text-muted m-b-md">New Quotes</p>
-						<div>
-							<span data-ui-jp="sparkline" data-ui-options="[9,2,5,5,7,4,4,3,2,2], {type:'line', height:20, width: '60', lineWidth:1, valueSpots:{'0:':'#818a91'}, lineColor:'#818a91', spotColor:'#818a91', fillColor:'', highlightLineColor:'rgba(120,130,140,0.3)', spotRadius:0}" class="sparkline inline"></span>
-						</div>
-					</div>
-				</div>
-	        </div>
-	        <div class="col-xs-6 col-sm-3 b-b">
-				<div class="padding">
-					<div>
-						<span class="pull-right"><i class="fa fa-caret-up text-primary m-y-xs"></i></span>
-						<span class="text-muted l-h-1x"><i class="ion-paper-airplane text-muted"></i></span>
-					</div>
-					<div class="text-center">
-						<h2 class="text-center _600">36</h2>
-						<p class="text-muted m-b-md">New Compains</p>
-						<div>
-							<span data-ui-jp="sparkline" data-ui-options="[3,3,1,62,4,3,7,3,2,5], {type:'line', height:20, width: '60', lineWidth:1, valueSpots:{'0:':'#818a91'}, lineColor:'#818a91', spotColor:'#818a91', fillColor:'', highlightLineColor:'rgba(120,130,140,0.3)', spotRadius:0}" class="sparkline inline"></span>
-						</div>
-					</div>
-				</div>
-	        </div>
-        </div>
+
 		<div class="padding">
 			<div class="row">
 			    <div class="col-sm-4">
--- src/views/uikit/ui.blade.php
+++ src/views/uikit/ui.blade.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
   <meta charset="utf-8" />
-  <title>aside - Bootstrap 4 web application</title>
+  <title>element</title>
   <meta name="description" content="Responsive, Bootstrap, BS4" />
   <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, minimal-ui" />
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -16,20 +16,7 @@
   <meta name="mobile-web-app-capable" content="yes">
   <link rel="shortcut icon" sizes="196x196" href="/element/images/logo.png">
   
-  <!-- style -->
-  <link rel="stylesheet" href="/element/css/animate.css/animate.min.css" type="text/css" />
-  <link rel="stylesheet" href="/element/css/glyphicons/glyphicons.css" type="text/css" />
-  <link rel="stylesheet" href="/element/css/font-awesome/css/font-awesome.min.css" type="text/css" />
-  <link rel="stylesheet" href="/element/css/material-design-icons/material-design-icons.css" type="text/css" />
-  <link rel="stylesheet" href="/element/css/ionicons/css/ionicons.min.css" type="text/css" />
-  <link rel="stylesheet" href="/element/css/simple-line-icons/css/simple-line-icons.css" type="text/css" />
-  <link rel="stylesheet" href="/element/css/bootstrap/dist/css/bootstrap.min.css" type="text/css" />
-
-  <!-- build:css css/styles/app.min.css -->
-  <link rel="stylesheet" href="/element/css/styles/app.css" type="text/css" />
-  <link rel="stylesheet" href="/element/css/styles/style.css" type="text/css" />
-  <!-- endbuild -->
-  <link rel="stylesheet" href="/element/css/styles/font.css" type="text/css" />
+  <link rel="stylesheet" href="/build/app.css" type="text/css" />
 </head>
 <body class="pace-done black" ui-class="black">
   <div class="app" id="app">
@@ -44,8 +31,7 @@
         <!-- brand -->
         <a href="index.html" class="navbar-brand">
         	<div data-ui-include="'images/logo.svg'"></div>
-        	<img src="/element/images/logo.png" alt="." class="hide">
-        	<span class="hidden-folded inline">aside</span>
+        	<img src="/element/brand.svg" alt=".">
         </a>
         <!-- / brand -->
       </div>
